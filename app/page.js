a2d085b3c83709b47c5a9bb02ddcdd6f28fc8ce2
Nishant <nissshhdev@gmail.com>
2024-07-21 13:37:56 +0530

    inititiated backend

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Analytics } from "@vercel/analytics/react"
 const page = () => {
   return (
-    <div className='h-screen flex flex-col justify-center items-center text-5xl border p-4 border-slate-800'>
-      bharosa
+    <div className='h-screen bg-slate-300  text-gray-900 flex flex-col justify-center items-center text-5xl border p-4 border-slate-800'>
+     <span className=' py-3  px-2 border border-gray-700 border-4 '>bharosa</span> 
       <span className='text-3xl'>trust/reviews</span>
       <div className='absolute bottom-4'>
         *work in progress- stay tuned
