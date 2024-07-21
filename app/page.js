0854fbf6494e6b2af8783cf8841e0527c5864940
Nishant <nissshhdev@gmail.com>
2024-07-21 12:47:24 +0530

    added general sans font

--- app/page.js
+++ app/page.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { Analytics } from "@vercel/analytics/react"
 const page = () => {
   return (
-    <div>page</div>
+    <div className='h-screen flex flex-col justify-center items-center text-5xl'>
+      bharosa
+      <span className='text-3xl'>trust.reviews</span>
+      </div>
   )
 }
 
