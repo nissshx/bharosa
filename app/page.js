2d498f0de0865a99699d0038ca63635c403645dd
Nishant <nissshhdev@gmail.com>
2024-07-21 12:50:16 +0530

    initiated project with all analytics setup

--- app/page.js
+++ app/page.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { Analytics } from "@vercel/analytics/react"
 const page = () => {
   return (
-    <div className='h-screen flex flex-col justify-center items-center text-5xl'>
+    <div className='h-screen flex flex-col justify-center items-center text-5xl border p-4 border-slate-800'>
       bharosa
-      <span className='text-3xl'>trust.reviews</span>
+      <span className='text-3xl'>trust/reviews</span>
+      <div className='absolute bottom-4'>
+        *work in progress- stay tuned
+      </div>
       </div>
   )
 }
